090993faecbef62d19edee32a6581d017f288cbf
Eduard Valentin <eduard.valentin1996@gmail.com>
2023-05-25 13:34:33 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var PROTO_PATH = __dirname + "/service.proto";
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
+
+// Suggested options for similarity to existing grpc.load behavior
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+	keepCase: true,
+	longs: String,
+	enums: String,
+	defaults: true,
+	oneofs: true,
+});
+
+var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+function getServer() {
+	var server = new grpc.Server();
+	server.addService(
+		protoDescriptor.com.example.v1.graphql.ExampleService.service,
+		{
+			createExample: (call, callback) => {
+				console.log("Received", call.request);
+				return callback(null, { field_one: 1 });
+			},
+		}
+	);
+	return server;
+}
+
+var server = getServer();
+server.bindAsync(
+	"localhost:50051",
+	grpc.ServerCredentials.createInsecure(),
+	() => {
+		console.log("server started");
+		server.start();
+	}
+);
